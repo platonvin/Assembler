1beeca028e609e7dc94df81cecf743656cd71cac
platonvin <109219241+mamobus@users.noreply.github.com>
2022-11-03 01:03:06 +0300

    Add files via upload

--- defines/commands.h
+++ defines/commands.h
@@ -1,23 +1,23 @@
 _CMD_DEF_(hlt, 0x00, 0, 0,
-printf("ABORT\n"); abort();
+printf("EXIT\n"); exit(0);
 )
 _CMD_DEF_(push, 0x01, 1, 0,
     if(is_reg) 
     {
-        temp += reg[code[i++]];
+        t1 += reg[code[i++]];
     }
     if(is_num) 
     {
-        temp += code[i++];
+        t1 += code[i++];
     }
 
     if(is_ram)
     {
-        stack_push(stack, ram[temp]);
+        stack_push(stack, ram[t1]);
     }
     else
     {
-        stack_push(stack, temp);
+        stack_push(stack, t1);
     }
 )
 _CMD_DEF_(pop, 0x02, 1, 0,
@@ -25,13 +25,13 @@ _CMD_DEF_(pop, 0x02, 1, 0,
     {
         if(is_reg) 
         {
-            temp += reg[code[i++]];
+            t1 += reg[code[i++]];
         }
         if(is_num)
         {
-            temp += code[i++];
+            t1 += code[i++];
         }
-        ram[temp] = stack_pop(stack);
+        ram[t1] = stack_pop(stack);
     }
     else
     {
@@ -47,61 +47,56 @@ _CMD_DEF_(pop, 0x02, 1, 0,
     }
 )
 _CMD_DEF_(add, 0x03, 0, 0,
-    temp += stack_pop(stack);
-    temp += stack_pop(stack);
-    stack_push(stack, temp);
+    t1 += stack_pop(stack);
+    t1 += stack_pop(stack);
+    stack_push(stack, t1);
 )
 _CMD_DEF_(sub, 0x04, 0, 0,
-    temp += stack_pop(stack);
-    temp -= stack_pop(stack);
-    stack_push(stack, temp);
+    t2 = stack_pop(stack);
+    t1 = stack_pop(stack);
+    stack_push(stack, t1 - t2);
 )
-_CMD_DEF_(imul, 0x05, 0, 0,
-    temp = stack_pop(stack);
-    temp *= stack_pop(stack);
-    stack_push(stack, temp);
+_CMD_DEF_(imul, 0x05, 0, 0, 
+    t1 = stack_pop(stack);
+    t1 *= stack_pop(stack);
+    stack_push(stack, t1);
 )
 _CMD_DEF_(mul, 0x06, 0, 0,
-    temp = stack_pop(stack);
-    temp *= stack_pop(stack);
-    if(temp < 0)
+    t1 = stack_pop(stack);
+    t1 *= stack_pop(stack);
+    if(t1 < 0)
     {
-        temp = -temp;
+        t1 *= -1;
     }
-    stack_push(stack, temp);
+    stack_push(stack, t1);
 )
 _CMD_DEF_(idiv, 0x07, 0, 0,
-    temp = stack_pop(stack);
-    temp /= stack_pop(stack);
-    stack_push(stack, temp);
+    t2 = stack_pop(stack);
+    t1 = stack_pop(stack);
+    stack_push(stack, t1 / t2);
 )
 _CMD_DEF_(div, 0x08, 0, 0,
-    temp = stack_pop(stack);
-    temp /= stack_pop(stack);
-    if(temp < 0)
+    t2 = stack_pop(stack);
+    t1 = stack_pop(stack);
+    t1 /= t2;
+    if(t1 < 0)
     {
-        temp = -temp;
+        t1 = -t1;
     }
-    stack_push(stack, temp);
+    stack_push(stack, t1);
 )
 _CMD_DEF_(out, 0x09, 0, 0,
-    temp = stack_pop(stack);
-    printf("%ld\n", temp);
+    t1 = stack_pop(stack);
+    printf("%ld\n", t1);
 )
-_CMD_DEF_(in, 0x0a, 1, 0,
-    scanf("%d", &temp);
-    if(is_reg)
-    {
-        reg[code[i++]] = temp;
-    }
-    else
-    {
-        stack_push(stack, temp);
-    }
+_CMD_DEF_(in, 0x0a, 0, 0,
+    scanf("%ld", &t1);                
+    stack_push(stack, t1);
 )
 _CMD_DEF_(jb, 0x0b, 1, 1, 
-    temp = stack_pop(stack);
-    if(temp < stack_pop(stack)) 
+    t2 = stack_pop(stack);
+    t1 = stack_pop(stack);
+    if(t1 < t2) 
     {
         i = code[i];
     }
@@ -111,8 +106,9 @@ _CMD_DEF_(jb, 0x0b, 1, 1,
     }
 )
 _CMD_DEF_(jbe, 0x0c, 1, 1, 
-    temp = stack_pop(stack);
-    if(temp <= stack_pop(stack)) 
+    t2 = stack_pop(stack);
+    t1 = stack_pop(stack);
+    if(t1 <= t2) 
     {
         i = code[i];
     }
@@ -122,8 +118,9 @@ _CMD_DEF_(jbe, 0x0c, 1, 1,
     }
 )
 _CMD_DEF_(ja, 0x0d, 1, 1, 
-    temp = stack_pop(stack);
-    if(temp > stack_pop(stack)) 
+    t2 = stack_pop(stack);
+    t1 = stack_pop(stack);
+    if(t1 > t2) 
     {
         i = code[i];
     }
@@ -133,8 +130,9 @@ _CMD_DEF_(ja, 0x0d, 1, 1,
     }
 )
 _CMD_DEF_(jae, 0x0e, 1, 1, 
-    temp = stack_pop(stack);
-    if(temp >= stack_pop(stack)) 
+    t2 = stack_pop(stack);
+    t1 = stack_pop(stack);
+    if(t1 >= t2) 
     {
         i = code[i];
     }
@@ -144,8 +142,9 @@ _CMD_DEF_(jae, 0x0e, 1, 1,
     }
 )
 _CMD_DEF_(je, 0x0f, 1, 1, 
-    temp = stack_pop(stack);
-    if(temp == stack_pop(stack)) 
+    t2 = stack_pop(stack);  
+    t1 = stack_pop(stack);
+    if(t1 == t2) 
     {
         i = code[i];
     }
@@ -155,8 +154,9 @@ _CMD_DEF_(je, 0x0f, 1, 1,
     }
 )
 _CMD_DEF_(jne, 0x10, 1, 1, 
-    temp = stack_pop(stack);
-    if(temp != stack_pop(stack)) 
+    t2 = stack_pop(stack);
+    t1 = stack_pop(stack);
+    if(t1 != t2) 
     {
         i = code[i];
     }
@@ -168,9 +168,26 @@ _CMD_DEF_(jne, 0x10, 1, 1,
 _CMD_DEF_(jmp, 0x11, 1, 1, 
     i = code[i++];
 )
-_CMD_DEF_(neg, 0x12, 1, 0, 
+_CMD_DEF_(neg, 0x12, 0, 0, 
     stack_push(stack, -stack_pop(stack));
 )
 _CMD_DEF_(sqrt, 0x13, 0, 0, 
     stack_push(stack, sqrt(stack_pop(stack)));
+)
+_CMD_DEF_(dup, 0x14, 0, 0, 
+    t1 = stack_pop(stack);
+    stack_push(stack, t1);
+    stack_push(stack, t1);
+)
+_CMD_DEF_(vpop, 0x15, 0, 0,
+    t2 = stack_pop(stack);
+    t1 = stack_pop(stack);
+    vram[t1] = t2;
+)
+_CMD_DEF_(vfl, 0x16, 0, 0,
+    for(size_t i = 0; (vram[i] != 0) && (i < MAX_VRAM); i++)
+    {
+        putchar(vram[i]);
+        vram[i] = 0;
+    }
 )
